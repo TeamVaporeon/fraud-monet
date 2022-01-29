1829a068070d5ef90066dcd8fafb8f1c36756424
John Razi <gc.johnrazi@gmail.com>
2022-01-29 11:57:34 -0800

    Added a bit of game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import GameLogic from './Components/GameLogic.jsx';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'></header>
+      <GameLogic />
     </div>
   );
 }
