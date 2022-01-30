78685d43dcd6ae54b296caef354084ffe21aa963
Kenneth Bakke <kennethsbakke@gmail.com>
2022-01-29 16:15:08 -0800

    Push it

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const controller = require('./controllers');
 
-router.get('/room/', controller.rooms.getHomePage);
-router.post('/room/', controller.rooms.generateRoomCode);
+router.get('/room', controller.rooms.getHomePage);
+router.post('/room', controller.rooms.generateRoomCode);
 //fraudmonet.com/room?[hash code here]
 router.get('/room/:id', controller.rooms.getRoomInfo);
 
