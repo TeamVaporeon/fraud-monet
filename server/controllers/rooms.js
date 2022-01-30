b32cd0c66bf94c842fe7fcdc4eaa90d94c7b20c7
Kenneth Bakke <kennethsbakke@gmail.com>
2022-01-29 16:59:45 -0800

    Add basic routings and organization

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -2,10 +2,26 @@
 module.exports = {
   getHomePage: async function(req, res) {
     console.log(req.url);
-    res.status(200).send('Fraud money home page!')
+    res.status(200).send('Fraud money home page!');
+  },
+  generateRoomCode: function(req, res) {
+    console.log(req.body);
+    // get random string
+    // hash string and save in HT
+    // save user as host
+    res.status(201).send('Fraud money generate code!');
   },
   getRoomInfo: async function(req, res) {
     console.log(req.params);
-    res.send(req.params.id);
-  })
+    res.status(200).send(req.params.id);
+  },
+  addUser: async function(req, res) {
+    res.status(201).send('User added');
+  },
+  updateUser: async function(req, res) {
+    res.status(204).send('Updated user');
+  },
+  getPrompt: async function(req, res) {
+    res.status(200).send('Here\'s your prompt');
+  }
 }
