8f09df6cb2e4061b8eee498d4e64a38216d8ff76
Joseph Corral <genobeam125@gmail.com>
2022-01-31 15:52:51 -0800

    Added connection between socket and CreateRoom, made some changes to App, server, and createroom

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import GameMain from './Components/GameMainPage/GameMain/GameMain.jsx';
 import makeRoomData from './mock-data.js';
 import { createContext } from 'react';
+import CreateRoom from './Components/CreateRoom.jsx'
 
 export const AppContext = createContext();
 
@@ -16,7 +17,8 @@ function App() {
     <AppContext.Provider value={{ dummyData }}>
       <div className='App'>
         <header className='App-header'></header>
-        <GameMain />
+        <CreateRoom/>
+        {/* <GameMain /> */}
         {/* <Players />
         <div>canvas goes here</div>
         <Chat /> */}
