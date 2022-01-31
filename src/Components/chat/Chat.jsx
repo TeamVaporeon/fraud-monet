c4b511028f02367c747e62d8b5766122d9c596ea
Wisdom Ibole <wisdom.ibole@gmail.com>
2022-01-31 12:19:29 -0800

    Minor changes after merge; deletes Chat.jsx

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import './Chat.css';
-// import io from 'socket.io-client';
-
-// const socket = io.connect('http://localhost:8080');
 
 const Chat = ({ socket, room, username }) => {
   const [currentMsg, setCurrentMsg] = useState('');
