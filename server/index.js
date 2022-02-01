919a994f4d2fd4138902196c3a1c815de55408a4
Kenneth Bakke <kennethsbakke@gmail.com>
2022-01-31 16:30:57 -0800

    Proper version of index

--- server/index.js
+++ server/index.js
@@ -14,10 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname + './build')));
-app.use('', router);
-=======
 app.use(express.static('build'));
 
 // Create Room Page
@@ -27,29 +23,6 @@ app.get('/', (req, res) => {
   // console.log('Made it to /: Cookie is ', newCookie);
   res.sendFile(path.join(__dirname + '../build/index.html'));
 });
-// Room endpoint
-app.use('/room', router);
->>>>>>> 8f09df6cb2e4061b8eee498d4e64a38216d8ff76
-
-// Live data
-// Countdown timer
-// Drawing
-// Voting
-// Chat
-//
-// Host get's cookie right away
-// Test host emit event on create game
-// Store first player in user data model
-// Redirect player to game room
-//
-// On non-host join
-// make get request -> checks for cookie
-  // if no cookie set on
-  // if cookie don't matter
-  // send html
-// Set up their connection via emit
-// on username submit -> store as new user
-
 // Room endpoint
 app.use('/room', router);
 
@@ -70,29 +43,10 @@ Promise.all([pubClient.connect(), subClient.connect()])
     io.listen(3000);
   })
 
-
 // On Client Connecting To Server
 io.on('connection', (socket) => {
 
   console.log(`Socket Connected With Id: `, socket.id);
-<<<<<<< HEAD
-  console.log(socket);
-
-  // Check cookies
-  const cookief = socket.handshake.headers.cookie;
-  const cookies = cookie.parse(socket.handshake.headers.cookie);
-  console.log(cookies);
-
-  // Join a room based on room id
-  socket.on('room', (room) => {
-    const subRoom = room.substr(5);
-    socket.join(subRoom);
-    socket.on('draw', (mouseData) => {
-      // Broadcast mouseData to all connected sockets
-      socket.broadcast.to(subRoom).emit('draw', mouseData);
-    });
-  });
-=======
   // Join a room based on room id
   socket.on('room', (url) => {
     socket.room = url.substr(5);
@@ -121,7 +75,6 @@ io.on('connection', (socket) => {
     socket.to(userMessage.room).emit('receive_message', userMessage);
   });
   /* ----- End of CHATROOM Code ----- */
->>>>>>> 8f09df6cb2e4061b8eee498d4e64a38216d8ff76
 
   // On user disconnecting
   socket.on('disconnect', () => {
