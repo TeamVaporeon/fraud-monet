c3331474d65c37eb1c7a9945471d72b7b0b8e0a1
Kavita <kavi.pdri@gmail.com>
2022-01-31 17:00:29 -0800

    react router implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-/*eslint no-unused-vars: 0*/
+/*eslint-disable no-unused-vars*/
 
 import './App.css';
 import GameMain from './Components/GameMainPage/GameMain/GameMain.jsx';
 import makeRoomData from './mock-data.js';
 import { createContext } from 'react';
-import CreateRoom from './Components/CreateRoom.jsx'
+import CreateRoom from './Components/CreateRoom.jsx';
+import { Link } from 'react-router-dom';
 
 export const AppContext = createContext();
 
@@ -17,15 +18,7 @@ function App() {
     <AppContext.Provider value={{ dummyData }}>
       <div className='App'>
         <header className='App-header'></header>
-<<<<<<< HEAD
         <GameMain />
-=======
-        <CreateRoom/>
-        {/* <GameMain /> */}
-        {/* <Players />
-        <div>canvas goes here</div>
-        <Chat /> */}
->>>>>>> 8f09df6cb2e4061b8eee498d4e64a38216d8ff76
       </div>
     </AppContext.Provider>
   );
--- src/Components/CreateRoom.jsx
+++ src/Components/CreateRoom.jsx
@@ -1,44 +1,60 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { io } from 'socket.io-client';
+import { Link } from 'react-router-dom';
 
 const getHome = async () => {
   const response = await fetch('http://localhost:8080');
   console.log(response);
-}
+};
 getHome();
 var socket = io();
 var generateRandString = () => {
   var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
-  for(var i = 0; i < 8; i++) {
-    result+= characters.charAt(Math.floor(Math.random() * charactersLength));
+  for (var i = 0; i < 8; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-
-}
-
+};
 
 var CreateRoom = (props) => {
-
   const [name, setName] = useState('');
-  return(
+  return (
     <div>
       <form>
         <label>
           Enter Your Username:
-          <input type="text" onChange={(e)=> {
-            setName(e.target.value);
-          }}/>
-          <button type="submit" text="Create Room" onClick={(e) => {
-            e.preventDefault();
-            var idString = generateRandString();
-            socket.emit('createRoom',{username: name, roomId: idString, color: '#000', host: true, fraud: false, role: "player"})
-            }}>Create Room </button>
+          <input
+            type='text'
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <button
+            type='submit'
+            text='Create Room'
+            onClick={(e) => {
+              e.preventDefault();
+              var idString = generateRandString();
+              socket.emit('createRoom', {
+                username: name,
+                roomId: idString,
+                color: '#000',
+                host: true,
+                fraud: false,
+                role: 'player',
+              });
+            }}
+          >
+            Create Room{' '}
+          </button>
         </label>
       </form>
+      <Link to='/play'>Play</Link>
     </div>
-  )
-}
+  );
+};
 
 export default CreateRoom;
