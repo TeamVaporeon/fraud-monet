9aa88bd2bd858a6679f2b01efc808bb83020dc39
Thanh Ha <thanh3203@gmail.com>
2022-02-01 16:29:16 -0800

    accepted changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,41 @@ import makeRoomData from './mock-data.js';
 import { useState, createContext, useEffect } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
+import { hostSocket } from './Components/CreateRoom';
 
 export const AppContext = createContext();
 
+var socket = io({
+  withCredentials: true,
+  autoConnect: false
+});
+
 function App() {
   const [round, setRound] = useState(0);
-  const [users, setUsers] = useState([]);
-
-  console.log('PATH', window.location.pathname);
-
-  const socket = io({
-    withCredentials: true,
-    autoConnect: false
+  const [users, setUsers] = useState(() => {
+    if (hostSocket.id) {
+      return [hostSocket.auth.user];
+    } else {
+      return [];
+    };
   });
-  socket.on('users', (users) => {
-    console.log('FRONT USERS', users);
-  });
-  socket.on('newUser', (user) => {
-    console.log('New User:', user);
-  })
-  const dummyData = makeRoomData();
   const [playerUsername, setPlayerUsername] = useState('');
 
-  // Initial 0; after clicked Start, 1; after first vote, 2; after second vote, 3 >> Game End, Show result modal
+  if (hostSocket.id) {
+    socket = hostSocket;
+  };
 
-  useEffect(() => {
-    axios.get(`/host${window.location.pathname}`)
-      .then(res => {
-        setUsers(res.data);
-      })
-      .catch(err => console.log(err.message));
-  }, [])
+  socket.on('users', (userList) => {
+    setUsers(userList);
+  });
+  socket.on('newUser', (newUsers) => {
+    setUsers(newUsers);
+  });
+
+
+  const dummyData = makeRoomData();
+
+  // Initial 0; after clicked Start, 1; after first vote, 2; after second vote, 3 >> Game End, Show result modal
 
   return (
     <AppContext.Provider
