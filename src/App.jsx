7df8de9174d6c5772eaabf1776d92642184dc2b1
Meilin <immeilin9@gmail.com>
2022-02-01 17:20:18 -0800

    added big update from be

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,27 @@ import makeRoomData from './mock-data.js';
 import { useState, createContext, useEffect } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
-import { hostSocket } from './Components/CreateRoom';
 
 export const AppContext = createContext();
 
-var socket = io({
-  withCredentials: true,
-  autoConnect: false
-});
-
 function App() {
   const [round, setRound] = useState(0);
-  const [users, setUsers] = useState(() => {
-    if (hostSocket.id) {
-      return [hostSocket.auth.user];
-    } else {
-      return [];
-    };
-  });
-  const [playerUsername, setPlayerUsername] = useState('');
+  const [users, setUsers] = useState([]);
 
-  if (hostSocket.id) {
-    socket = hostSocket;
-  };
+  console.log('PATH', window.location.pathname);
 
-  socket.on('users', (userList) => {
-    setUsers(userList);
+  const socket = io({
+    withCredentials: true,
+    autoConnect: false,
   });
-  
-  socket.on('newUser', (newUsers) => {
-    setUsers(newUsers);
+  socket.on('users', (users) => {
+    console.log('FRONT USERS', users);
   });
-
+  socket.on('newUser', (user) => {
+    console.log('New User:', user);
+  });
   const dummyData = makeRoomData();
+  const [playerUsername, setPlayerUsername] = useState('');
 
   // Initial 0; after clicked Start, 1; after first vote, 2; after second vote, 3 >> Game End, Show result modal
 
@@ -50,6 +38,7 @@ function App() {
       })
       .catch((err) => console.log(err.message));
   }, []);
+
   return (
     <AppContext.Provider
       value={{
@@ -65,7 +54,7 @@ function App() {
     >
       <div className='App'>
         <header className='App-header'></header>
-        <GameMain dummyData={dummyData} actualData={users}/>
+        <GameMain dummyData={dummyData} actualData={users} />
       </div>
     </AppContext.Provider>
   );
