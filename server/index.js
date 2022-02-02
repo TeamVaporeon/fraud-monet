e02c10aca48f13856e61fa5917f8e6e6e94c1d34
John Razi <gc.johnrazi@gmail.com>
2022-02-02 12:32:34 -0800

    color order in server

--- server/index.js
+++ server/index.js
@@ -10,24 +10,24 @@ const rooms = {};
 const defaultColors = {
   '#FFCCEB': true, //Cotton Candy
   '#DF6770': true, //Candy Pink
+  '#ff69b4': true, //Hot Pink
   '#EA9F4E': true, //Sandy Brown
+  '#a52a2a': true, //Brown
+  '#ff0000': true, //Red
+  '#ffa500': true, //Orange
   '#FBE89B': true, //Green Yellow Crayola
+  '#ffff00': true, //Yellow
+  '#00ff00': true, //Lime
   '#B9E49F': true, //Granny Smith Apple
+  '#008000': true, //Green
   '#73E5DA': true, //Turquoise
   '#94B1E9': true, //Wild Blue Yonder
+  '#0000ff': true, //Blue
+  '#4b0082': true, //Indigo
+  '#800080': true, //Purple
   '#AE97CD': true, //Wisteria
   '#D9ABD6': true, //Lilac
   '#A9B3BF': true, //Cadet Blue Crayola
-  '#ff0000': true, //Red
-  '#0000ff': true, //Blue
-  '#008000': true, //Green
-  '#ffa500': true, //Orange
-  '#800080': true, //Purple
-  '#a52a2a': true, //Brown
-  '#ffff00': true, //Yellow
-  '#4b0082': true, //Indigo
-  '#ff69b4': true, //Hot Pink
-  '#00ff00': true, //Lime
 };
 
 // Data JSON File
