0ab5dd6ff3a6d1da1843d35ac723be1079e7b22e
Kenneth Bakke <kennethsbakke@gmail.com>
2022-02-03 17:19:09 -0800

    create boilerplate socket helper functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,8 +2,20 @@ const { InMemorySessionStore } = require('../sessionStore');
 const sessionStore = new InMemorySessionStore();
 
 module.exports = {
-  saveUser: function(socket) {
+  saveUser: function(socket, callback) {
     console.log(socket);
+  },
+  initializeUser: function(socket, url, callback) {
+    socket.room = url;
+    socket.join(socket.room);
+    socket.user ? socket.user.id = socket.id : socket.user = {};
+    console.log(`Socket Connected With Id: `, socket.id);
+  },
+  ifUserInSession: function(user) {
+    let session = sessionStore.findSession(user.id);
+    if (session) {
+      return session;
+    }
   },
   sessionStore: sessionStore,
 }
