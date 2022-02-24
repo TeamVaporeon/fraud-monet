4bcb5a330d3997101b446aa88d1b6749fdfd9c2b
Kenneth Bakke <kennethsbakke@gmail.com>
2022-02-23 17:20:37 -0800

    Removed dev folder

--- serve/adapter.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('redis');
-
-const pubClient = createClient({ host: 'localhost', port: 6379});
-const subClient = pubClient.duplicate();
-
-module.exports = {
-  pubClient: pubClient,
-  subClient: subClient,
-}
